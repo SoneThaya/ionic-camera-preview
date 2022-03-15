918c5f22ac11dc6794a8d217fe2922d8616e4fec
sone <sthaya@gmail.com>
2022-03-15 11:02:17 -0700

    ionic camera preview with custom buttons

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ionic Camera Preview with custom buttons
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { CameraPreview } = Plugins;
+import {
+  CameraPreviewOptions,
+  CameraPreviewPictureOptions,
+} from '@capacitor-community/camera-preview';
+
+// needed for web registeration
+import '@capacitor-community/camera-preview';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +15,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  image = null;
+  cameraActive = false;
 
   constructor() {}
 
+  openCamera() {
+    const cameraPreviewOptions: CameraPreviewOptions = {
+      position: 'rear',
+      parent: 'cameraPreview',
+      className: 'cameraPreview',
+    };
+    CameraPreview.start(cameraPreviewOptions);
+    this.cameraActive = true;
+  }
+
+  async stopCamera() {
+    await CameraPreview.stop();
+    this.cameraActive = false;
+  }
+
+  async captureImage() {
+    const cameraPreviewPictureOptions: CameraPreviewPictureOptions = {
+      quality: 90,
+    };
+
+    const result = await CameraPreview.capture(cameraPreviewPictureOptions);
+    this.image = `data:image/jpeg;base64,${result.value}`;
+    this.stopCamera();
+  }
+
+  flipCamera() {
+    CameraPreview.flip();
+  }
 }
